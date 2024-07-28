a88638e079fbbc2c300391b37b7be615303566d2
evgenrudenko22 <125033944+evgenrudenko22@users.noreply.github.com>
2024-07-28 15:49:38 +0300

    Init project

---
+++ backend/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+import random
+
+app = FastAPI()
+
+@app.get("/items")
+def get_items():
+    items = [
+        {
+            "id": 1,
+            "name": "Capibara"
+        },
+        {
+            "id": 2,
+            "name": "Sniffer"
+        }
+    ]
+
+    random.shuffle(items)
+    return items
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import styles from "./App.module.css"
+
+function App() {
+  const [items, setItems] = useState(null)
+
+  const fetchItems = () => {
+    axios.get('http://localhost:8000/items').then(r => {
+      setItems(r.data)
+    })
+  }
+
+  useEffect(() => {
+    fetchItems()
+    setInterval(() => {
+      fetchItems()
+    }, 2000)
+  }, [])
+
+  return (
+    <>
+      <div className={styles.items}>
+        {items && items.map(item => {
+          return <span className={styles.item} key={item.id}>
+            <span>{item.name}</span>
+          </span>
+        })}
+      </div>
+    </>
+  )
+}
+
+export default App
